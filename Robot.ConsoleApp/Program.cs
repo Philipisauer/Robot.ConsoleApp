f2481d363865455d84dd57d5e542ec8a59b790c7
Philipi Sauer <philipisauer09@gmail.com>
2025-03-27 15:33:58 -0300

    Adicionar arquivos de projeto.

---
+++ Robot.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Robot.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("digite a area do Robô: ");
+                int area = Convert.ToInt32(Console.ReadLine());
+
+                int numeroDeCasas;
+
+                numeroDeCasas = area * area;
+
+                Console.WriteLine($"Area é de: {numeroDeCasas} casas.");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
